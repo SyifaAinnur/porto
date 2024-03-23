b3b5fd1e7a92457797c26a920567f217bc666214
Lailatul Fitriyah <arwahitem01@gmail.com>
2024-03-23 23:12:04 +0700

    add preload

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,8 +1,34 @@
+"use client";
 
-export default function LandingPage() {
+import { useEffect, useState } from 'react';
+import { AnimatePresence } from 'framer-motion';
+import Preload from '@/components/preload';
+
+export default function Home() {
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect( () => {
+      (
+        async () => {
+            const LocomotiveScroll = (await import('locomotive-scroll')).default
+            const locomotiveScroll = new LocomotiveScroll();
+  
+            setTimeout( () => {
+              setIsLoading(false);
+              document.body.style.cursor = 'default'
+              window.scrollTo(0,0);
+            }, 2000)
+        }
+      )()
+    }, [])
+
+
   return (
-    <>
-      <p>asdsadasda</p>
-    </>
+    <main>
+        <AnimatePresence mode='wait'>
+            {isLoading && <Preload />}
+        </AnimatePresence>
+    </main>
   );
 }
